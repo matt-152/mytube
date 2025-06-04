f2878647cdcb0301501595ec94d9d291c9cafe2c
Matthew Delaney <MattDelaney_948@protonmail.com>
2025-06-04 18:09:14 -0500

    Experimenting w/ Header CSS
    Started work on the header. Learning CSS as I go, experimenting w/ grid
    and flexbox layouts.

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>mytube</title>
         <link rel="stylesheet" href="/styles.css" />
+        <link rel="stylesheet" href="/main.css" />
       </head>
       <body>
         <Component />
