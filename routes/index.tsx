c435c78fc9a356a809ce7909680fdc2ff941a8db
Matthew Delaney <MattDelaney_948@protonmail.com>
2025-06-05 15:33:59 -0500

    Nav bar in satisfactory state
    Nav bar is in a satisfactory form for now. Will move on to other
    elements. Plan is to add greater sophistication later.
    
    Some annoyances with plain CSS. It fails silently; even on a syntax
    error like a missing semicolon won't come up in either Fresh's console
    or the browser inspector. Even if there was a build step to catch this,
    a naming error (PageNave instead of PageNav) might still be missed. I'm
    sure there's a linter which could pick up on this.
    
    Will take quite a while before this app is impressive. Over time this
    will become a great asset, but real webdevs would use more tooling to
    increase productivity. Might switch over to an even simpler, more
    cookie-cutter app to get something on CV.

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,12 +4,17 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <header class="pagenav">
-      <div class="pagenav-start">
+    <header class="PageNav">
+      <div class="PageNav-start">
           MyTube
       </div>
-      <div class="pagenav-mid"></div>
-      <div class="pagenav-end"></div>
+      <div class="PageNav-mid">
+        <div class="PageNav-mid-search">
+          <input type="text" placeholder="Search"></input>
+          <button>Go</button>
+        </div>
+      </div>
+      <div class="PageNav-end"></div>
     </header>
   );
 }
