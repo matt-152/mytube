05e581aff3af258fe3812d1cb85e221e261793f2
Matthew Delaney <MattDelaney_948@protonmail.com>
2025-06-06 17:37:27 -0500

    Video Card roughed out
    Basic structure of a VideoCard component has been roughed out and is
    looking good. At some point I'll need to factor these bits out into
    their own components.
    
    Took a few shortcuts to get the look I wanted. Wonder how hard they'll
    be to fix when actual functionality is added.

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,17 +4,32 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <header class="PageNav">
-      <div class="PageNav-start">
-          MyTube
-      </div>
-      <div class="PageNav-mid">
-        <div class="PageNav-mid-search">
-          <input type="text" placeholder="Search"></input>
-          <button>Go</button>
+    <div>
+      <header class="PageNav">
+        <div class="PageNav-start">
+            MyTube
+        </div>
+        <div class="PageNav-mid">
+          <div class="PageNav-mid-search">
+            <input type="text" placeholder="Search"></input>
+            <button>Go</button>
+          </div>
+        </div>
+        <div class="PageNav-end"></div>
+      </header>
+      <div class="VideoCard">
+        <div class="VideoCard-thumbnail" style="background-color: #444444" />
+        <div class="VideoCard-info">
+          <div class="VideoCard-info-left">
+            <div/>
+          </div>
+          <div class="VideoCard-info-right">
+            <h4>My Awesome Video</h4>
+            <p>Content Creator</p>
+            <p>105K views - 5 hours ago</p>
+          </div>
         </div>
       </div>
-      <div class="PageNav-end"></div>
-    </header>
+    </div>
   );
 }
