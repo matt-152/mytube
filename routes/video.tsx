2793d3a228bca69787aa0021d7882a109ff8f606
Matthew Delaney <MattDelaney_948@protonmail.com>
2025-06-11 18:24:48 -0500

    Continued work on Video Page
    Continued work on the video page, was a bit distracted today so I didn't
    get much done. Just positioned the mock video and the title.
    
    One thing I am keeping in mind moving forward is semantic HTML. The
    mock video is in an `article`, the title in a `footer` of that
    `article`. Important for SEO and is an alternative to `div` soup.

--- routes/video.tsx
+++ routes/video.tsx
@@ -1,7 +1,12 @@
 export default function Video() {
   return (
     <main>
-      <div class="Video" src=""/>
+      <article class="Video">
+        <div class="VideoMock" src=""/>
+        <footer>
+          <h1>My Awesome Video</h1>
+        </footer>
+      </article>
     </main>
   );
 }
